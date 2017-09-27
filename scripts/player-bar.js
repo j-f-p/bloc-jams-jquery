f957a26d8cfd160c03a3507d35d68110d2c85883
JF Padilla <emailreel@gmail.com>
2017-09-27 14:31:13 -0400

    add script to enable the next track button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,4 +3,12 @@ $(document).ready( function() {
     player.playPause();
     $(this).attr('playState',player.playState);
   });
+
+  $('button#next').click( function() {
+    if(player.playState=='playing' &&
+        album.songs.indexOf(player.currentlyPlaying) < album.songs.length-1 )
+      player.playPause(
+        album.songs[album.songs.indexOf(player.currentlyPlaying) + 1]
+      );
+  });
 });
