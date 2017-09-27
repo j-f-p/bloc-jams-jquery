f2301e7e5f1ebfa560de4516710d84643be9a902
JF Padilla <emailreel@gmail.com>
2017-09-27 14:48:56 -0400

    add script to enable the previous track button (modeule 2 assignment 13)

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,11 +1,19 @@
 $(document).ready( function() {
+  $('button#previous').click( function() {
+    if( player.playState=='playing' &&
+        album.songs.indexOf(player.currentlyPlaying) > 0 )
+      player.playPause(
+        album.songs[album.songs.indexOf(player.currentlyPlaying) - 1]
+      );
+  });
+
   $('button#play-pause').click( function() {
     player.playPause();
     $(this).attr('playState',player.playState);
   });
 
   $('button#next').click( function() {
-    if(player.playState=='playing' &&
+    if( player.playState=='playing' &&
         album.songs.indexOf(player.currentlyPlaying) < album.songs.length-1 )
       player.playPause(
         album.songs[album.songs.indexOf(player.currentlyPlaying) + 1]
